b7740537867cb71db889fa3cdebfc2904ca5cc42
정지원 <4dakto@gmail.com>
2017-08-23 22:01:33 +0900

    Make Dispatching Redux Actions More Reliable With Action Creators

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import store from './store';
+import { updateCurrent } from './reducers/todo';
 
 const todoChangeHandler = val =>
-  store.dispatch({ type: 'CURRENT_UPDATE', payload: val });
+  store.dispatch(updateCurrent(val));
 
 const render = () => {
   const state = store.getState();
