321c1638cbfa773acb8e69884d9b160d5f2b47cc
정지원 <4dakto@gmail.com>
2017-08-28 23:02:01 +0900

    Configure and Use Redux Dev Tools

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,32 +1,39 @@
-import {getTodos} from '../lib/todoService';
+import { getTodos, createTodo} from '../lib/todoService'
 
 const initState = {
   todos: [],
   currentTodo: ''
 };
 
-const TODO_ADD = 'TODO_ADD';
+const TODO_ADD = 'TODO_ADD'
 const TODOS_LOAD = 'TODOS_LOAD'
-const CURRENT_UPDATE = 'CURRENT_UPDATE';
+const CURRENT_UPDATE = 'CURRENT_UPDATE'
 
-export const updateCurrent = (val) => ({ type: CURRENT_UPDATE, payload: val });
+export const updateCurrent = (val) => ({ type: CURRENT_UPDATE, payload: val })
 export const loadTodos = (todos) => ({ type: TODOS_LOAD, payload: todos })
+export const addTodo = (todo) => ({ type: TODO_ADD, payload: todo })
 export const fetchTodos = () => {
   return (dispatch) => {
     getTodos()
       .then(todos => dispatch(loadTodos(todos)))
   }
 }
+export const saveTodo = (name) => {
+  return (dispatch) => {
+    createTodo(name)
+      .then(res => dispatch(addTodo(res)))
+  }
+}
 
 export default (state = initState, action) => {
   switch (action.type) {
     case TODO_ADD:
-      return { ...state, todos: state.todos.concat(action.payload) };
+      return { ...state, todos: state.todos.concat(action.payload) }
     case TODOS_LOAD:
       return {...state, todos: action.payload}
     case CURRENT_UPDATE:
-      return { ...state, currentTodo: action.payload };
+      return { ...state, currentTodo: action.payload }
     default:
-      return state;
+      return state
   }
-};
+}
